924768042e3d6dc692512f3be02165284fa4aeff
Saeed Rasooli <saeed.gnu@gmail.com>
2020-08-16 07:27:27 +0430

    update urbandictionary.py

--- urbandictionary.py
+++ urbandictionary.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2020 Saeed Rasooli <saeed.gnu@gmail.com> (ilius)
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. Or on Debian systems, from /usr/share/common-licenses/GPL
+# If not, see <http://www.gnu.org/licenses/gpl.txt>.
+
 
 import os
 from os import makedirs, listdir
@@ -7,36 +25,61 @@ import requests
 import html
 import re
 from hashlib import sha1
-from time import sleep
+from time import sleep, strftime
 from queue import Queue
 from threading import Thread
+import socket
+import gzip
+
+
+def print_tm(msg):
+	print(f"{strftime('%F %T')} {msg}")
+
+
+useAPI = False
+
+host = "urbandictionary.com"
+apiHost = "api.urbandictionary.com"
+
+#host = socket.gethostbyname(host)
+print_tm(f"Using host {host}")
+
 
 homeDir = os.getenv("HOME")
-cacheDir = join(homeDir, ".cache", "urbandictionary")
+
+if os.sep == "/":
+	cacheDir = "/media/D/urbandictionary"
+elif os.sep == "\\":
+	cacheDir = "D:\\urbandictionary"
+else:
+	raise OSError("could not detect OS")
+
 
 browseDir = join(cacheDir, "browse")
-dictDir = join(cacheDir, "dict")
+dictDir = join(cacheDir, "dict.crawler")
 
 if not isdir(browseDir):
 	makedirs(browseDir)
 if not isdir(dictDir):
 	makedirs(dictDir)
 
-browseFirstWord = "https://www.urbandictionary.com/browse.php?word=a"
-
 def fetchURL(url: str):
 	while True:
 		try:
 			res = requests.get(url)
 		except requests.exceptions.ConnectionError as e:
-			print(f"failed: {e} for {url}")
+			print_tm(f"failed: {e} for {url}")
 			sleep(0.5)
 			continue
 		if res.ok:
 			return res.text
-		print(f"failed: {res} for {url}")
-		# sc = res.status_code
-		#if sc >= 400 and sc < 500:
+		print_tm(f"failed: {res} for {url}")
+		sc = res.status_code
+		if sc == 403:
+			sleep(5)
+			continue
+		if sc >= 400 and sc < 500:
+			return f"status_code={sc}"
 		return
 		#sleep(0.1)
 
@@ -65,14 +108,14 @@ def fetchBrowse(word: str) -> "Tuple[str, str]":
 	if isfile(fpath):
 		with open(fpath, "r", encoding="utf-8") as _file:
 			text = _file.read()
-		print(f"loaded: {fpath} for word {word}")
+		print_tm(f"loaded: {fpath} for word {word}")
 	else:
 		text = fetchURL(
-			f"https://www.urbandictionary.com/browse.php?word={word}",
+			f"https://{host}/browse.php?word={word}",
 		)
 		with open(fpath, "w", encoding="utf-8") as _file:
 			_file.write(text)
-		print(f"saved: {fpath} for word {word}")
+		print_tm(f"saved: {fpath} for word {word}")
 	nextWord = nextWordFromBrowseText(text)
 	return text, nextWord
 
@@ -100,20 +143,23 @@ def filePathFromWord(b_word: bytes) -> str:
 
 def downloadWord(word: str):
 	fpath = join(dictDir, filePathFromWord(word.encode("utf-8")))
-	if isfile(fpath):
-		#print(f"file exists: {fpath}")
-		return
+	if isfile(fpath + ".gz") or isfile(fpath):
+		#print_tm(f"file exists: {fpath}")
+		return 0
 	text = fetchURL(
-		f"https://www.urbandictionary.com/define.php?term={word}",
+		f"http://{apiHost}/v0/define?term={word}"
+		if useAPI else
+		f"https://{host}/define.php?term={word}"
 	)
 	if text is None:
-		return
+		return 0
 	parent = dirname(fpath)
 	if not isdir(parent):
 		makedirs(parent)
-	with open(fpath, mode="w", encoding="utf-8") as _file:
+	with gzip.open(fpath + ".gz", mode="wt", encoding="utf-8") as _file:
 		_file.write(text)
-	#print(f"file saved: {fpath}")
+	#print_tm(f"file saved: {fpath}")
+	return 1
 
 
 re_define = re.compile(
@@ -125,30 +171,31 @@ def fetchWordsOfBrowseFile(fname, workerI):
 		text = _file.read()
 	count = 0
 	for word in re_define.findall(text):
-		downloadWord(html.unescape(word))
-		count += 1
-	print(
-		f'worker{workerI}: downloaded {count} words'
-		f' from "{bytes.fromhex(fname).decode("utf-8")}"'
-	)
+		count += downloadWord(html.unescape(word))
+	if count > 0:
+		print_tm(
+			f'worker{workerI}: downloaded {count} words'
+			f' from "{bytes.fromhex(fname).decode("utf-8")}"'
+		)
 
 
 def workerLoop(workerI, q):
-    """This is the worker thread function.
-    It processes items in the queue one after
-    another.  These daemon threads go into an
-    infinite loop, and only exit when
-    the main thread ends.
-    """
-    while True:
-        #print(f'{workerI}: Looking for the next enclosure')
-        browseFname = q.get()
-        #print(f'{workerI}: Downloading: {browseFname}')
-        fetchWordsOfBrowseFile(browseFname, workerI)
-        q.task_done()
+	"""This is the worker thread function.
+	It processes items in the queue one after
+	another.  These daemon threads go into an
+	infinite loop, and only exit when
+	the main thread ends.
+	"""
+	while True:
+		try:
+			browseFname = q.get()
+			fetchWordsOfBrowseFile(browseFname, workerI)
+			q.task_done()
+		except Exception as e:
+			print_tm(e)
 
 
-num_workers = 64
+num_workers = 8
 max_queue_size = num_workers * 2
 queue = Queue(maxsize=max_queue_size)
 
@@ -160,6 +207,6 @@ if __name__ == "__main__":
 		worker.start()
 	for fname in listdir(browseDir):
 		queue.put(fname)
-		#print(f"queue size: {queue.qsize()}")		
+		#print_tm(f"queue size: {queue.qsize()}")		
 
 
